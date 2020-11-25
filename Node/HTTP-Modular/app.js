91dd62a15634570eb7d747e23bf23e30df4f10a9
Tyler Nguyen <nguyn.tyler@gmail.com>
2020-11-24 20:45:33 -0500

    node

---
+++ Node/HTTP-Modular/app.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const generateContent = require("./modules/generateContent");
+const serveFile = require("./modules/serveFile");
+const port = 6677;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return serveFile(req, res, req.url);
+  if (req.url === "/styles.css") return serveFile(req, res, req.url);
+
+  res.writeHead(200);
+  const url = new URL(req.headers.host + req.url);
+  let page = url.searchParams.get("page");
+  let count = url.searchParams.get("count");
+
+  let additional = [];
+  url.searchParams.forEach((value, name) => {
+    additional.push(`<li>${name}:${value}</li>`);
+  });
+
+  let wrapper = generateContent({
+    page,
+    count,
+    additional,
+  });
+
+  res.write(wrapper);
+  res.end();
+});
+
+server.listen(port);
