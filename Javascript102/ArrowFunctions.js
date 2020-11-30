3621cb260b74a08bfcc6e073823bb6dbb643d60f
Tyler Nguyen <nguyn.tyler@gmail.com>
2020-11-29 21:39:50 -0500

    edits

--- Javascript102/ArrowFunctions.js
+++ Javascript102/ArrowFunctions.js
@@ -1,24 +1,24 @@
 // Write an arrow function that accepts a number as an argument and multiplys that number by itself and returns the value.
-const timesTwo = (arg) => arg * 2
+const timesTwo = (arg) => arg * 2;
 
-console.log(timesTwo(8))
+console.log(timesTwo(8));
 
 // ----------------------
-// Write an arrow function that has 2 arguments. Add the two arguments and console.log the results.
+// Write an arrow function that has 2 arguments. Add the two arguments and console.log the results..
 
-const addNums = (num1, num2) => num1 + num2
+const addNums = (num1, num2) => num1 + num2;
 
-console.log(addNums(8, 5))
+console.log(addNums(8, 5));
 
 // ----------------------
 // Write an array of names.
 // using forEach loop through the array and have the callbakc function be an arrow function that prints will print the index +1 and then the name.
 
-myArr = ['Tyler', 'Nguyen', 'Tony', 'Banana']
+myArr = ["Tyler", "Nguyen", "Tony", "Banana"];
 
-const myForEachFunc = (letter, idx) => console.log(`${idx + 1}. ${letter}`)
+const myForEachFunc = (letter, idx) => console.log(`${idx + 1}. ${letter}`);
 
-myArr.forEach(myForEachFunc)
+myArr.forEach(myForEachFunc);
 
 // // ----------------------
 
